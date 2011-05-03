d72105e4e7bd35b0a273d3cf25b797772c2da541
dahool <gabriel.sgt@gmail.com>
2011-05-02 21:16:49 -0300

    better version parsing

--- src/jipdbs/web/InfoServlet.java
+++ src/jipdbs/web/InfoServlet.java
@@ -19,8 +19,6 @@ import com.google.appengine.api.utils.SystemProperty;
 @SuppressWarnings("serial")
 public class InfoServlet extends HttpServlet {
 
-	private static final String MAJOR_VERSION = "0";
-	
 	public static class App {
 
 		final String version;
@@ -39,10 +37,11 @@ public class InfoServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		DateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm");
-		String[] ver = StringUtils.split(MAJOR_VERSION + "." + SystemProperty.applicationVersion.get(), ".");
+		String[] ver = StringUtils.split(SystemProperty.applicationVersion.get(), ".");
 		long longversion = (long) (Long.parseLong(ver[ver.length-1]) / Math.pow(2, 28)); 
 		Date d = new Date(longversion * 1000);
-		ver[ver.length-1] = format.format(d); 
+		ver[ver.length-1] = format.format(d);
+		ver[0] = ver[0].replace('-', '.'); 
 		App app = new App(Functions.join(ver, "."));
 		req.setAttribute("app", app);
 	}
