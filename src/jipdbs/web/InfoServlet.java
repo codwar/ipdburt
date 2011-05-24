ceeb8199747f8f72392e541a621529af615d3160
dahool <gabriel@opensuse.org>
2011-05-24 14:06:20 -0300

    fix issue with baninfo parsing

--- src/jipdbs/web/InfoServlet.java
+++ src/jipdbs/web/InfoServlet.java
@@ -12,8 +12,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import jipdbs.util.Functions;
 
-import org.datanucleus.util.StringUtils;
-
 import com.google.appengine.api.utils.SystemProperty;
 
 @SuppressWarnings("serial")
@@ -37,7 +35,7 @@ public class InfoServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		DateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm");
-		String[] ver = StringUtils.split(SystemProperty.applicationVersion.get(), ".");
+		String[] ver = SystemProperty.applicationVersion.get().split(".");
 		long longversion = (long) (Long.parseLong(ver[ver.length-1]) / Math.pow(2, 28)); 
 		Date d = new Date(longversion * 1000);
 		ver[ver.length-1] = format.format(d);
