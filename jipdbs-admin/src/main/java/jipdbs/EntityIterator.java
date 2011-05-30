dd4d75547ccac7262c4a910a5ce81244dcd2505b
dahool <gabriel@opensuse.org>
2011-05-30 09:36:59 -0300

    initial commit of admin

---
+++ jipdbs-admin/src/main/java/jipdbs/EntityIterator.java
@@ -0,0 +1,59 @@
+package jipdbs;
+
+import com.google.appengine.api.datastore.Cursor;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.QueryResultList;
+
+public final class EntityIterator {
+
+	public static interface Callback {
+
+		void withEntity(Entity entity, DatastoreService ds) throws Exception;
+
+	}
+
+	public static void iterate(String entityName, long maxElements,
+			int batchSize, Callback callback) {
+
+		batchSize = Math.min(batchSize, 1000);
+
+		DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
+		Query q = new Query(entityName);
+		PreparedQuery pq = ds.prepare(q);
+
+		QueryResultList<Entity> list = pq
+				.asQueryResultList(FetchOptions.Builder.withLimit(batchSize));
+
+		long totalElements = list.size();
+
+		Cursor cursor = list.getCursor();
+
+		while (totalElements < maxElements && list.size() > 0) {
+
+			for (Entity entity : list) {
+
+				try {
+					callback.withEntity(entity,
+							DatastoreServiceFactory.getDatastoreService());
+				} catch (Exception e) {
+					break;
+				}
+			}
+
+			list = pq.asQueryResultList(FetchOptions.Builder.withLimit(
+					batchSize).startCursor(cursor));
+			totalElements += list.size();
+			cursor = list.getCursor();
+		}
+	}
+
+	public static void iterate(String entityName, long maxElements,
+			Callback callback) {
+		iterate(entityName, maxElements, 100, callback);
+	}
+}
