d6acdbd27b6c3d6a0253bc444caa209e70e53a93
dahool <gabriel@opensuse.org>
2012-02-09 09:48:44 -0300

    minor changes

--- sql/update_0_9-0_10.sql
+++ sql/update_0_9-0_10.sql
@@ -1,4 +1,4 @@
-alter table alias drop column textindex;
-alter table alias drop column normalized;
-alter table alias add index (nickname);
-alter table alias add column nameindex varchar(225) COLLATE utf8_unicode_ci NOT NULL;
+alter table alias drop column textindex,
+                    drop column normalized,
+                    add column nameindex varchar(225) COLLATE utf8_unicode_ci NOT NULL,
+                    add index (nickname);
